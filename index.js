7a8fae9a51a9f2dbdcb881ddd810e27c9c4be130
Hui Lin (Lynn) Olson <f_huilino@marvin.com>
2022-02-03 23:06:49 -0600

    made some changes

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
-console.log('hello');
+let question = prompt ("What is your name? ");
+console.log (question);
+
+If (question ==='Do you have a good day?'){
+    console.log("Yes");
+} else{
+    console.log('No');
+}
