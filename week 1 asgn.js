c1470ee59f198a5118b0abca808902438d13da67
Hui Lin (Lynn) Olson <f_huilino@marvin.com>
2022-02-07 09:55:05 -0600

    Added assignment

---
+++ week 1 asgn.js
@@ -0,0 +1,165 @@
+// let x = 8;
+// let y = 12;
+// x = y + x;
+// let z = x + y;
+// console.log(z);
+
+// do we have enough money for plane ticket and vacation time
+// if yes to both we can go vacation
+
+// let planeTicket = 150;
+// let money = 450;
+// let vacationTime = false
+
+
+// if (money >= planeTicket && vacationTime) {
+
+// console.log("true");
+
+// } else {
+//     console.log("false");
+// }
+ 
+// let num1 = 1;
+// let num2 = 2;
+// let num3 = 3;
+// let num4 = 4;
+
+// if(num1>=num2){
+//     if(num3>=num4){
+//         console.log("Both statements are correct.");
+//     }else{
+//             console.log("The first statement is true, but the second is not.")
+//         }
+//     } else{
+        
+//     }       
+//     console.log("Program ended.");
+
+// let numberOfDay = 11;
+// switch(numberOfDay){
+//     case 1:
+//         console.log('Sunday');
+//         break;
+//     case 2:
+//         console.log('Monday');
+//         break;
+//     case 3:
+//         console.log('Tuesday');
+//         break;
+//     case 4:
+//         console.log('Wednesday');
+//         break;
+//     case 5:
+//         console.log('Thursday');
+//         break;
+//     case 6:
+//         console.log('Friday');
+//         break;
+//     case 7:
+//         console.log('Satuday');
+//         break;
+//     default:
+//     console.log("The number of day is invalid.");
+// }
+
+// let myNumber = 2;
+// let num1 = 2;
+
+// if(myNumber>num1){
+//     console.log(num1 +' is smaller than my number, ' + myNumber);
+// } else if (myNumber<num1){
+//         console.log(num1 + ' is larger than my number, ' + myNumber);
+// } else{
+//         console.log(num1 + ' is equal to my number, ' + myNumber);
+//     }
+
+// let numberGrade = 42;
+// let letterGrade;  
+
+// if(numberGrade >= 90 ){
+//         console.log("A");
+//     }else if(numberGrade >= 80 && numberGrade<90){
+//         console.log("B");
+//     }else if(numberGrade >=70 && numberGrade<80){
+//         console.log("C");
+//     }else if(numberGrade >=60 && numberGrade<70){
+//         console.log("D");
+//     }else{
+//         console.log("F");
+//     }
+    
+// let userNumber = 120;
+// let counter = userNumber;
+// if (userNumber<=100 && userNumber>=1){
+//     while (counter <=100) {
+//         console.log(counter++);
+//     }
+// } else{
+//     console.log("wrong");
+// }
+
+// Create a do/while loop that will print the userNumber and then add 6 after each loop. Stop the loop once the number is greater than or equal to 100.
+// let userNumber = 50;
+
+// do{
+ 
+//  console.log(userNumber) ;
+//  userNumber +=6;
+// } while (userNumber <=100);
+
+// let requiredCupsOfRice = 5;
+// let currentCupsOfRice = 0;
+// // for(currentCupsOfRice; currentCupsOfRice <=requiredCupsOfRice ; currentCupsOfRice++){
+// //     console.log('The bowl contains ' + currentCupsOfRice + 'cups of rice');
+// // }
+// //  console.log('We have enough rice.');
+
+// //  while(currentCupsOfRice != requiredCupsOfRice){
+// //     console.log('The bowl contains ' + (++currentCupsOfRice) + ' cups of rice.');
+// // }
+// // console.log('We have enough rice!');
+
+// do{
+//     console.log('The bowl contains ' + currentCupsOfRice++ + ' cups of rice. ');
+
+// } while (currentCupsOfRice !== requiredCupsOfRice);
+
+// console.log('We have enough rice!');
+
+// Write a for loop that prints every third number from 0 up to, and including, 99.
+
+// let x;
+// for (x=0 ; x<=99 ; x+=3){
+//      console.log(x);
+   
+//  }
+//  console.log('Bravo.');
+
+// for(var i = 0; i <= 99; i++){
+//     if(i % 3 === 0){
+//         console.log(i)
+//     }
+// }
+
+// let i=0;
+// do{
+//     if(i%3 == 0){
+//         console.log(i);
+//     } 
+//     i++;
+// } while (i<=99)
+
+
+for(let i=1; i<=100 ; i++){
+    if(i % 2 == 0){
+         console.log(i + ' foo');
+     }
+    
+ }
+ console.log("good job")  
+
+ 
+
+
+
